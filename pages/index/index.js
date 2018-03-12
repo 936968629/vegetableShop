359c2a88a939103b8189c2e11d42464be8bf6fef
pythonic <936968629@qq.com>
2018-03-12 18:04:50 +0800

    wen

--- pages/index/index.js
+++ pages/index/index.js
@@ -16,7 +16,7 @@ Page({
   onLoad: function (options) {
     this._loadData();
   },
-
+  //加载幻灯片
   _loadData:function(){
     index.getBannerData(1,(res)=>{
       console.log(res);
@@ -24,8 +24,23 @@ Page({
         'bannerData':res
       });
     })
+  },
+  //加载最热商品
+  _loadHotProduct:function(){
+
+  },
+
+  onPullDownRefresh:function(){
+    // wx.stopPullDownRefresh();
+    console.log(1);
+    wx.showToast({
+
+      title: '没事儿别乱拉',//提示信息
+
+      icon: 'success',//成功显示图标
+
+      duration: 1000//时间
+
+    })
   }
-
-
-
 })
