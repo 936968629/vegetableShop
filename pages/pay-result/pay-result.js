5ec2cf9e536f3994abe5ee38d20ec59c3cc07a53
pythonic <936968629@qq.com>
2018-04-16 14:59:28 +0800

    wen
    支付结果

--- pages/pay-result/pay-result.js
+++ pages/pay-result/pay-result.js
@@ -12,55 +12,17 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    // console.log(options.from);
+    this.setData({
+      payResult: options.flag,
+      id: options.id,
+      from: options.from
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  //查看订单
+  viewOrder:function(event){
+    wx.navigateBack({
+      delta: 1
+    })
   }
 })
