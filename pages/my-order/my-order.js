b98a23a299d8698937d0ba2ee184d1f8667bd0cf
pythonic <936968629@qq.com>
2018-04-23 17:02:04 +0800

    wen

--- pages/my-order/my-order.js
+++ pages/my-order/my-order.js
@@ -27,7 +27,13 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+    wx.showNavigationBarLoading()
+    //模拟加载
+    setTimeout(function () {
+      // complete
+      wx.hideNavigationBarLoading() //完成停止加载
+      wx.stopPullDownRefresh() //停止下拉刷新
+    }, 1000);
   },
 
   /**
