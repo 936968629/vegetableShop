1966d461c383fa6a412f8abb69f82c7e1dded863
251328109 <251328109@qq.com>
2018-05-01 18:22:37 +0800

    wen
    订单计时

--- pages/my-order/my-order.js
+++ pages/my-order/my-order.js
@@ -7,8 +7,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-    pageIndex:1,
+    pageIndex:0,
     orderArr:[],
+    nodata:false,
+    timeArr:[],
   },
 
   /**
@@ -27,16 +29,35 @@ Page({
   _getOrders: function (callback) {
     myOrder.getOrders(this.data.pageIndex, (res) => {
       var data = res.data;
+      var tiArr = {};
       console.log(data)
+      
       if (data.length > 0) {
         this.data.orderArr.push.apply(this.data.orderArr, data);
+
+        // for (let i in this.data.orderArr){
+        //   var currentData = this.data.orderArr[i];
+        //   if(currentData['status'] == 1){
+        //     tiArr[i] = currentData['end_time']
+        //   }
+        // }
+        // this.tfunc(this.data.orderArr);
         this.setData({
           orderArr: this.data.orderArr
         });
+        var timer = setInterval(this.tfunc, 1000);
       }
       else {
-        this.data.isLoadedAll = true;
+        console.log("nodata")
+        this.setData({
+          nodata:true
+        })
       }
+      console.log(tiArr)
+      
+      this.setData({
+        pageIndex:this.data.pageIndex+1,
+      })
       callback && callback();
     })
   },
@@ -53,12 +74,54 @@ Page({
       wx.stopPullDownRefresh() //停止下拉刷新
     }, 1000);
   },
-
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+    this._getOrders();
   },
+  //时间处理函数
+  tfunc:function(){
+    var tiArr = {};
+    var ntim = new Date(); //当前时间戳 
+    for (let i in this.data.orderArr){
+      let timstr;
+      var currentData = this.data.orderArr[i];
+      if(currentData['status'] == 1){
+        tiArr[i] = currentData['end_time']
+        timstr = currentData['end_time']
+        var _tm = timstr.replace(/-/g, '\/'); //目标时间字段串里的-替换成/,格式化的需要 
+        var ftim = new Date(_tm); //格式化目标时间 
+        var rs = this.timGap(ntim.getTime(), ftim.getTime()); //调用取时间差方法 
+        //var _str = rs.d + '天' + rs.h + '小时' + rs.m + '分钟' + rs.s + '秒'; //将返回的数据拼接字符串
+        var _str = rs.h + ':' + rs.m + ':' + rs.s;
+        this.data.orderArr[i]['timedown'] = _str;
+      }
+    }
+    this.setData({
+      orderArr: this.data.orderArr
+    });
+  },
+  //取时间差方法 
+  timGap:function (ntim, ftim) {
+    var date3 = ftim - ntim; //时间差值毫秒数 
+    var days = Math.floor(date3 / (24 * 3600 * 1000)); //取天数 
+    var level1 = date3 % (24 * 3600 * 1000);//取天数后剩下的毫秒数 
+    var hours = Math.floor(level1 / (3600 * 1000)); //取小时数 
+    var level2 = level1 % (3600 * 1000);//取小时后剩下的毫秒数 
+    var minutes = Math.floor(level2 / (60 * 1000));//取分钟 
 
+    var level3 = level2 % (60 * 1000);//取分钟后剩下的毫秒数 
+    var seconds = Math.floor(level3 / 1000);//取秒 
+    //定义对象 
+    var tim = {};
+    //赋值 
+    tim['d'] = days;
+    tim['h'] = hours;
+    tim['m'] = minutes;
+    tim['s'] = seconds;
+    return tim; //返回数据 
+  } 
+
+  
 })
