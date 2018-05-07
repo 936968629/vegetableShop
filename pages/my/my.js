78b727f34553e7df48629b0ce2c41acb20ac060c
pythonic <936968629@qq.com>
2018-05-07 17:58:56 +0800

    wen

--- pages/my/my.js
+++ pages/my/my.js
@@ -41,7 +41,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+    this._getmessCount();
   },
   //跳转到我的订单
   toMyorder:function(event){
@@ -72,5 +72,9 @@ Page({
         });
       }
     })
+  },
+  //获取消息数量
+  _getmessCount:function(){
+    
   }
 })
